9148853fa17d9280190f51dea3738d2c81159c4b
Holly Drazenovich <hollydraze14@gmail.com>
2022-10-15 20:30:14 -0500

    added basic functions to each view

---
+++ src/components/EditPost/EditPost.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function EditPost() {
+    return (
+      <div className="container">
+        <div>
+          <h1>This is a filler header
+            <button>New Post</button>
+          </h1>
+        </div>
+      </div>
+    );
+  }
+
+  export default EditPost;
---
+++ src/components/PersonalProfile/PersonalProfile.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function PersonalProfile() {
+    return (
+      <div className="container">
+        <div>
+          <h1>This is a filler header
+            <button>New Post</button>
+          </h1>
+        </div>
+      </div>
+    );
+  }
+
+  export default PersonalProfile;
