e4ac7bd4e38c5cbcc0363281f7822644032f4872
Holly Drazenovich <hollydraze14@gmail.com>
2022-10-15 20:27:04 -0500

    added a basic header to home page to house new post button

--- server/server.js
+++ server/server.js
@@ -28,7 +28,7 @@ app.use('/api/user', userRouter);
 app.use(express.static('build'));
 
 // App Set //
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5008;
 
 /** Listen * */
 app.listen(PORT, () => {
---
+++ src/components/HomePage/HomePage.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function HomePage() {
+    return (
+      <div className="container">
+        <div>
+          <h1>This is my header for record Scratch
+            <button>New Post</button>
+          </h1>
+        </div>
+      </div>
+    );
+  }
+  
+  export default HomePage;
