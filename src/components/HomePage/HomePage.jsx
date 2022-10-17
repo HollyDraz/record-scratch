c5a4b42b6acff795230b612c9d8a27f8dec459c3
Holly Drazenovich <hollydraze14@gmail.com>
2022-10-17 17:38:30 -0500

    added filler card to home pafe

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,8 +1,53 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import {useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react';
+import axios from 'axios'
+// mui imports 
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 function HomePage() {
 const history = useHistory();
+
+const bull = (
+  <Box
+    component="span"
+    sx={{ display: 'inline-block', mx: '2px', transform: 'scale(0.8)' }}
+  >
+    
+  </Box>
+
+);
+
+const card = (
+  <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        Word of the Day
+      </Typography>
+      <Typography variant="h5" component="div">
+        be{bull}nev{bull}o{bull}lent
+      </Typography>
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+        adjective
+      </Typography>
+      <Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+    </CardContent>
+    <CardActions>
+      <Button size="small">Learn More</Button>
+    </CardActions>
+  </React.Fragment>
+);
+
 
     return (
       <div className="container">
@@ -10,6 +55,11 @@ const history = useHistory();
           <h1>This is my header for record Scratch <br />
             <button onClick={() => history.push('/create')}>New Post</button>
           </h1>
+          <Box sx={{ minWidth: 275 }}>
+          <Card variant="outlined">{card}</Card>
+          </Box>
+
+
           
         </div>
       </div>
