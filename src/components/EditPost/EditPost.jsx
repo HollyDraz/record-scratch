a355d8443ee7753316411a8cd1ce3b371c3db027
Holly Drazenovich <hollydraze14@gmail.com>
2022-10-20 11:05:03 -0500

    added form to edit page

--- src/components/EditPost/EditPost.jsx
+++ src/components/EditPost/EditPost.jsx
@@ -1,17 +1,37 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
+import {useSelector, useDispatch } from 'react-redux'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 
 
 function EditPost() {
   const history = useHistory();
+  const dispatch = useDispatch();
+  const [editTitle, setNewTitle] = useState();
+  const [editArtist, setNewArtist] = useState();
+  const [editAlbum, setNewAlbum] = useState();
+  const [editDescription, setNewDescription] = useState();
+
+
+ 
+  
+
+
     return (
       <div className="container">
         <div>
-          <h1>This is a filler header
-            <button>New Post</button>
+          <h1>Edit Post <br />
             <button onClick={() => history.push('/homepage')}>Home Page</button>
-            <button> DELETE </button>
           </h1>
+          <form >
+          <input onChange={(e) => setNewTitle(e.target.value)} defaultValue={editTitle} type="text" name="song" placeholder='song' />
+          <input onChange={(e) => setNewAlbum(e.target.value)} defaultValue={editArtist} type="text" name="album" placeholder='album' />
+          <input onChange={(e) => setNewArtist(e.target.value)} defaultValue={editAlbum} type="text" name="artist" placeholder='artist' />
+          <input onChange={(e) => setNewDescription(e.target.value)} defaultValue={editDescription} type="text" name="description" placeholder='description' />
+          </form>
+          <button>Publish</button>
         </div>
       </div>
     );
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -24,7 +24,7 @@ function Nav() {
         {/* If a user is logged in, show these links */}
         {user.id && (
           <>
-            <Link className="navLink" to="/user">
+            <Link className="navLink" to="/homepage">
               Home
             </Link>
 
