63e8f72df03ad47dcf0a11047dbf0da48675f37b
Holly Drazenovich <hollydraze14@gmail.com>
2022-10-27 22:53:07 -0500

    added a popover event to record png may be remvoed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,34 +4,70 @@ import {useSelector, useDispatch } from 'react-redux'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 // MUI 
-import { grey } from '@mui/material/colors';
-import { red } from '@mui/material/colors';
+import Popover from '@mui/material/Popover';
 import Typography from '@mui/material/Typography';
-import AlbumIcon from '@mui/icons-material/Album';
 import IconButton from '@mui/material/IconButton';
 import { Grid } from '@mui/material';
 
 function Header(){
     const history = useHistory();
+
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handlePopoverOpen = (event) => {
+      setAnchorEl(event.currentTarget);
+    };
+  
+    const handlePopoverClose = () => {
+      setAnchorEl(null);
+    };
+  
+    const open = Boolean(anchorEl);
+
     return(
         <div>
-             <Typography align='center'><h1> Welcome To Record Scratch!</h1> <br /> </Typography>
+             <Typography align='center'> </Typography>
              <Grid container direction="row" justifyContent="center" alignItems="flex-start" >
                 {/* align buttons  */}
-             <IconButton  size="large"  onClick={() => history.push('/info')}> About 
-                <AlbumIcon sx={{ color: grey[900], fontSize: 100 }} ></AlbumIcon>
+               
+             
+        <img src="record1.png" width="300" height="300" onClick={() => history.push('/info')}         onMouseEnter={handlePopoverOpen}
+        onMouseLeave={handlePopoverClose}/>
+        <Popover
+        id="mouse-over-popover"
+        sx={{
+          pointerEvents: 'none',
+        }}
+        open={open}
+        anchorEl={anchorEl}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'left',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'left',
+        }}
+        onClose={handlePopoverClose}
+        disableRestoreFocus >
+            About
+            
+             </Popover>
+         
+
+             <IconButton>
+                create 
+             <img src="record2.png" width="300" height="300" onClick={() => history.push('/create')}/>
              </IconButton>
 
-             <IconButton onClick={() => history.push('/create')}  size="large" > New Post  
-                <AlbumIcon sx={{  fontSize: 100 }}></AlbumIcon>
+             <IconButton>
+                Home page 
+             <img src="record3.png" width="300" height="300" onClick={() => history.push('/homepage')}/>
              </IconButton>
 
-             <IconButton  size="large"  onClick={() => history.push('/homepage')}> Homepage 
-                <AlbumIcon sx={{ color: grey[900], fontSize: 100 }} ></AlbumIcon>
-             </IconButton>
-
-             <IconButton size="large" > Log Out 
-                <AlbumIcon onClick={() => history.push('/login')} sx={{ color: red[900], fontSize: 100 }}></AlbumIcon>
+             <IconButton>
+                Logout
+             <img src="scratch.png" width="300" height="300" onClick={() => history.push('/login')}/>
              </IconButton>
 
 
