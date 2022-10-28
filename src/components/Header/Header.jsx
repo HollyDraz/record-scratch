12393a9c431ec427fdf083f8c89279a940d92b13
Holly Drazenovich <hollydraze14@gmail.com>
2022-10-28 11:55:27 -0500

    removed popeover elemetn

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,74 +4,26 @@ import {useSelector, useDispatch } from 'react-redux'
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 // MUI 
-import Popover from '@mui/material/Popover';
+
 import Typography from '@mui/material/Typography';
-import IconButton from '@mui/material/IconButton';
 import { Grid } from '@mui/material';
 
 function Header(){
-    const history = useHistory();
 
-    const [anchorEl, setAnchorEl] = React.useState(null);
 
-    const handlePopoverOpen = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
-  
-    const handlePopoverClose = () => {
-      setAnchorEl(null);
-    };
-  
-    const open = Boolean(anchorEl);
+
+
+
 
     return(
         <div>
-             <Typography align='center'> </Typography>
-             <Grid container direction="row" justifyContent="center" alignItems="flex-start" >
-                {/* align buttons  */}
-               
-             
-        <img src="record1.png" width="300" height="300" onClick={() => history.push('/info')}         onMouseEnter={handlePopoverOpen}
-        onMouseLeave={handlePopoverClose}/>
-        <Popover
-        id="mouse-over-popover"
-        sx={{
-          pointerEvents: 'none',
-        }}
-        open={open}
-        anchorEl={anchorEl}
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'left',
-        }}
-        transformOrigin={{
-          vertical: 'top',
-          horizontal: 'left',
-        }}
-        onClose={handlePopoverClose}
-        disableRestoreFocus >
-            About
-            
-             </Popover>
-         
+         <Grid container direction="row" justifyContent="center" alignItems="flex-start" >
+            <img src="record1.png" width="200" height="200" onClick={() => history.push('/info')} />
+            <img src="record2.png" width="200" height="200" onClick={() => history.push('/create')} />
+            <img src="record3.png" width="200" height="200" onClick={() => history.push('/homepage')}/>
+            <img src="scratch.png" width="200" height="200" onClick={() => history.push('/login')}/>
 
-             <IconButton>
-                create 
-             <img src="record2.png" width="300" height="300" onClick={() => history.push('/create')}/>
-             </IconButton>
-
-             <IconButton>
-                Home page 
-             <img src="record3.png" width="300" height="300" onClick={() => history.push('/homepage')}/>
-             </IconButton>
-
-             <IconButton>
-                Logout
-             <img src="scratch.png" width="300" height="300" onClick={() => history.push('/login')}/>
-             </IconButton>
-
-
-             </Grid>
+        </Grid>
             
 
 
