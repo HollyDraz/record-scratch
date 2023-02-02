763649b651fed98b17d40533baaac91b5e442c23
Holly Drazenovich <hollydraze14@gmail.com>
2023-02-02 12:01:23 -0600

    added imports

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -3,4 +3,10 @@
 
 
 //imports here 
+import React from 'react';
+import { Link } from 'react-router-dom';
+import LogOutButton from '../LogOutButton/LogOutButton';
+import './Nav.css';
+import { useSelector } from 'react-redux';
+
 
