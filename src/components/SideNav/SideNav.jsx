0ba31017612d0d6e59ea807de9e118b3a8791f90
Holly Drazenovich <hollydraze14@gmail.com>
2023-02-06 15:01:11 -0600

    added function to the side nav page

--- src/components/SideNav/SideNav.jsx
+++ src/components/SideNav/SideNav.jsx
@@ -6,14 +6,46 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
 import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
 
 // function 
-function SideNav () {
 
 
-}
+const drawerWidth = 240;
+
+function SideNav(props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  
+  }
+};
+
+const drawer = (
+    <div>
+        <Toolbar/>
+        <Divider/>
+        <List>
+            
+        </List>
+
+
+    </div>
+)
 
 
 export default SideNav;
